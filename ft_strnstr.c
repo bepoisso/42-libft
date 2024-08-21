9a5f5679393e7637fac00f8bc7e3c21b255ab531
bepoisso <poisson.benjamin69@gmail.com>
2024-08-21 19:02:30 +0200

    Fix ft_strnstr read to many byte little[j]

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: bepoisso <bepoisso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 15:16:23 by bepoisso          #+#    #+#             */
-/*   Updated: 2024/08/20 17:43:17 by bepoisso         ###   ########.fr       */
+/*   Updated: 2024/08/21 19:01:27 by bepoisso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	while (big[i] && i < len)
 	{
 		j = 0;
-		while (big[i + j] == little[j] && i + j < len)
+		while (little[j] && big[i + j] == little[j] && i + j < len)
 			j++;
 		if (little[j] == '\0')
 			return ((char *)big + i);
