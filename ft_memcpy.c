011236ef82f7de7f953505ed8d18feda6098dcfe
bepoisso <poisson.benjamin69@gmail.com>
2024-08-21 18:50:57 +0200

    Update norm ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: bepoisso <bepoisso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 17:40:42 by bepoisso          #+#    #+#             */
-/*   Updated: 2024/08/21 18:08:12 by bepoisso         ###   ########.fr       */
+/*   Updated: 2024/08/21 18:50:37 by bepoisso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t		i;
 	const char	*ptr_src;
-	char	*ptr_dest;
+	char		*ptr_dest;
 
 	i = 0;
 	ptr_src = (const char *)src;
